612772efeb90b918abf79801d16d1d500990d825
JoakimEtzell <92052164+JoakimEtzell@users.noreply.github.com>
2022-01-11 14:29:32 +0100

    Animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -7,4 +7,24 @@ public class Animal {
         this.weight = weight;
         this.hungry = hungry;
     }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHungry() {
+        return hungry;
+    }
+
+    public void setHungry(boolean hungry) {
+        this.hungry = hungry;
+    }
+
+
+
+
 }
